036f89b01ff4ad24978cc2c68f464398482215aa
baltner <baltner@d5dab3f4-42ea-4442-856c-254fa8de8673>
2008-06-24 15:59:48 +0000

    Replaces MySqlStrategy (wrong case)
    git-svn-id: svn://svn.sef.hq.nasa.gov/svn_git/java_library/java/dbutils/trunk@621 d5dab3f4-42ea-4442-856c-254fa8de8673

---
+++ src/gov/nasa/hq/sql/MySQLStrategy.java
@@ -0,0 +1,28 @@
+//$Id: $
+package gov.nasa.hq.sql;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MySQLStrategy implements DBStrategy {
+
+
+	public String getRegexp(String fieldname, Object value, boolean caseSensitive ) {
+	  //implements MySQL's regular expression for word boundaries
+
+        //MySQL regular expression for word boundary
+		StringBuffer buf = new StringBuffer();
+		buf.append(fieldname + " RLIKE ");
+    	buf.append("'[[:<:]]");
+    	buf.append(value.toString() );
+    	buf.append("[[:>:]]'");                 
+    	return new String( buf );    
+		
+	}
+	
+	public DateFormat formatDate(Date date) {
+		//TBD
+		return null;
+	}
+	
+}
